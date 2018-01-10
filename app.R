8074cac31d5a2e8fa95aaf09a25e82a03a1deabb
drgmulder <dr.g.mulder@gmail.com>
2018-01-10 22:34:24 +0100

    Updated with expected difference plot.

--- app.R
+++ app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(multicon)
 
 # Define UI 
 ui <- fluidPage(
@@ -26,7 +27,7 @@ ui <- fluidPage(
                                          max = 1000),
                             numericInput("mu2",
                                          "Mean population 2:",
-                                         100,
+                                         107.5,
                                          min = 1,
                                          max = 1000),
                             numericInput("sd",
@@ -53,7 +54,10 @@ ui <- fluidPage(
                             
                   ) #end panel
   ), #end first column 
-  column(8, verbatimTextOutput("ans")
+  column(8, tabsetPanel(
+    tabPanel("Required Sample size", verbatimTextOutput("ans")),
+    tabPanel("Expected Results", plotOutput("expResults"))
+  ) # end tabsetPanel
   ) # end column
   ) #end second fluidRow
 ) #end fluidPage
@@ -76,102 +80,6 @@ calcMOE.assu <- function(n, sd, assu) {
 } 
 
 
-#diffPLot function from multicon-package 
-#Ryne A. Sherman 
-#Maintainer: Ryne A. Sherman <rsherm13@fau.edu> 
-#Compiler: David G. Serfass <dserfass@fau.edu>
-
-diffPlot <-- function (x, y, plotFUN = mean, errFUN = c("ci", "se", "sd"), 
-                       conf = 0.95, grp.names = NULL, var.equal = FALSE, paired = FALSE, 
-                       ylim = NULL, ...) 
-{
-  se <- function(x) {
-    x <- na.omit(x)
-    res <- sqrt(var(x)/length(x))
-    res
-  }
-  ci <- function(x) {
-    x <- na.omit(x)
-    alpha <- 1 - (1 - conf)/2
-    res <- qt(alpha, length(x) - 2, lower.tail = T) * se(x)
-    res
-  }
-  if (!is.null(errFUN)) {
-    errFUN <- match.arg(errFUN)
-  }
-  dat <- data.frame(x, y)
-  dat <- na.omit(dat)
-  if (paired == FALSE) {
-    res <- tapply(dat[, 2], dat[, 1], plotFUN)
-    res <- c(res, res[2])
-    Ns <- tapply(dat[, 2], dat[, 1], length)
-    Vars <- tapply(dat[, 2], dat[, 1], var)
-    if (var.equal == FALSE) {
-      poolSE <- sqrt(sum(Vars/Ns))
-      df <- (sum(Vars/Ns)^2)/(sum(Vars^2/(Ns^2 * (Ns - 
-                                                    1))))
-    }
-    if (var.equal == TRUE) {
-      poolSE <- sqrt(sum((Ns - 1) * Vars)/(sum(Ns - 1))) * 
-        sqrt(sum(1/Ns))
-      df <- sum(Ns - 1)
-    }
-    diffBar <- ifelse(errFUN == "ci", qt(1 - (1 - conf)/2, 
-                                         df, lower.tail = T) * poolSE, ifelse(errFUN == "se", 
-                                                                              poolSE, ifelse(errFUN == "sd", sqrt(sum(Vars)), 0)))
-    e <- c(tapply(dat[, 2], dat[, 1], errFUN), diffBar)
-  }
-  if (paired == TRUE) {
-    res <- apply(dat, 2, plotFUN)
-    res <- c(res, res[2])
-    N <- nrow(dat)
-    Vars <- apply(dat, 2, var)
-    diffSE <- sd(dat[, 1] - dat[, 2])/sqrt(N)
-    df <- N - 1
-    diffBar <- ifelse(errFUN == "ci", qt(1 - (1 - conf)/2, 
-                                         df, lower.tail = T) * diffSE, ifelse(errFUN == "se", 
-                                                                              diffSE, ifelse(errFUN == "sd", sd(dat[, 1] - dat[, 
-                                                                                                                               2]), 0)))
-    e <- c(apply(dat, 2, errFUN), diffBar)
-  }
-  e <- ifelse(res <= 0, -e, e)
-  if (!is.null(ylim)) {
-    lims <- ylim
-  }
-  else {
-    lims <- c(min(res + e, res - e) - abs(0.4 * min(res + 
-                                                      e, res - e)), max(res + e, res - e) + 0.4 * abs(max(res + 
-                                                                                                            e, res - e)))
-  }
-  plot(c(1, 2, 4), res, pch = c(19, 19, 17), xaxt = "n", xlim = c(0.4, 
-                                                                  0.4 + 4), ylim = c(lims), bty = "l", ...)
-  if (!is.null(errFUN)) {
-    arrows(c(1, 2, 4), res + e, c(1, 2, 4), res - e, angle = 90, 
-           code = 3, length = 0.08)
-    arrows(2, res[2], 4.4, res[3], angle = 90, code = 3, 
-           length = 0, lty = 2)
-    arrows(1, res[1], 4.4, res[1], angle = 90, code = 3, 
-           length = 0, lty = 2)
-    if (res[1] >= res[2]) {
-      val <- axTicks(4) - axTicks(4)[min(which(axTicks(4) > 
-                                                 median(axTicks(4))))]
-      loc <- axTicks(4) - (axTicks(4)[min(which(axTicks(4) > 
-                                                  median(axTicks(4))))] - res[1])
-    }
-    if (res[1] < res[2]) {
-      val <- axTicks(4) - axTicks(4)[max(which(axTicks(4) < 
-                                                 median(axTicks(4))))]
-      loc <- axTicks(4) - (axTicks(4)[max(which(axTicks(4) < 
-                                                  median(axTicks(4))))] - res[1])
-    }
-    axis(4, at = loc, labels = val, ...)
-  }
-  if (is.null(grp.names)) {
-    grp.names <- c("x", "y")
-  }
-  axis(1, at = c(1, 2, 4), labels = c(grp.names, "Difference"))
-}
-
 # Define server logic 
 
 server <- function(input, output) {
@@ -180,11 +88,12 @@ server <- function(input, output) {
   sd = 15
   x <- seq(-4*sd+mu1, 4*sd+mu2, length=200)
   
-  output$distPlot <- renderPlot(plot(x, 
+  output$distPlot <- renderPlot({plot(x, 
                                      dnorm(x, 100, 15), 
                                      type="l", 
                                      ylab="Density", 
-                                     main="Populations"))
+                                     main="Populations")
+                                points(x, dnorm(x, 107.5, 15), lty=3, type="l")})
   #wait for setting population values and update plot
   observeEvent(input$draw, {
     mu1 <- isolate(input$mu1)
@@ -220,6 +129,14 @@ server <- function(input, output) {
                                        ylab="Density",
                                        xlab="Sample difference between means",
                                        main="Sampling Distribution of Difference"))
+    
+    g1 <- rnorm(ceiling(answer))
+    g1 <- g1*sd + mu1
+    g2 <- g1 + diff
+    dep <- c(g1, g2)
+    ind <- rep(c(1, 2), each=length(g1))
+    
+    output$expResults <- renderPlot(diffPlot(ind, dep, xlab=""))
   
     }) #end observeEvent2 
     
