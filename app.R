d16fb81ab150d674c545435ec2595f2f5ef13721
drgmulder <dr.g.mulder@gmail.com>
2018-01-10 13:03:11 +0100

    Changed title

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Planning for Precision"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
